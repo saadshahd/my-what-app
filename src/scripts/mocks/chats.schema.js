346a002c817620c69b9684edded97f446cba934b
Saad Shahd <dev.saad.shahd@gmail.com>
2016-08-14 09:42:36 +0200

    Add Chats list fake data

---
+++ src/scripts/mocks/chats.schema.js
@@ -0,0 +1,68 @@
+angular
+  .module('app')
+  .constant('chatsSchema', {
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'number',
+          chance: {
+            natural: {
+              min: 1,
+              max: 99
+            }
+          }
+        },
+        participants: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                chance: 'name'
+              },
+              avatar: {
+                type: 'url',
+                chance: {
+                  url: {
+                    domain: 'placeimg.com',
+                    path: '/32/32/any?'
+                  }
+                }
+              }
+            },
+            required: [
+              'name',
+              'avatar'
+            ]
+          },
+          minItems: 1,
+          maxItems: 8,
+          uniqueItems: true
+        },
+        lastMessage: {
+          type: 'string',
+          chance: {
+            sentence: {
+              words: 5
+            }
+          }
+        },
+        lastActive: {
+          type: 'date',
+          chance: 'date'
+        }
+      },
+      required: [
+        'id',
+        'participants',
+        'lastMessage',
+        'lastActive'
+      ]
+    },
+    minItems: 6,
+    maxItems: 24,
+    uniqueItems: true
+  });
